02f8af09b580f29a0c27e5f23dee21340ec3a682
sabrinapea <sabrinapea@yahoo.com>
2021-03-02 00:42:46 -0800

    Odd-or-Even app

---
+++ beginning/js/04-odd-or-even.js
@@ -0,0 +1,9 @@
+/* esline-env browser */
+
+for (let i = 0; i <= 15; i++) {
+  if (i % 2 == 0) {     
+      window.console.log(i + ' is an even number.');
+  } else {
+    window.console.log(i + ' is an odd number.');
+  }
+}
