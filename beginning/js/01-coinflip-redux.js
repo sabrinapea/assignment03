c828f7cbc547adf982902d3ddc7d950d7fabd415
sabrinapea <sabrinapea@yahoo.com>
2021-03-01 22:17:37 -0800

    Coinflip - redux app

---
+++ beginning/js/01-coinflip-redux.js
@@ -0,0 +1,10 @@
+let coinFlip;
+let choice = parseInt(prompt('Enter the number of times to flip'));
+for (let i = 0; i < choice; i++) {
+  coinFlip = Math.round(Math.random());
+  if (coinFlip === 0) {
+      window.console.log("Heads");
+  } else {
+      window.console.log("Tails")
+  }
+}12
