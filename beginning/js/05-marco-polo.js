20e0f3d97c1f52d7276b8b12abd890ee94b048bf
sabrinapea <sabrinapea@yahoo.com>
2021-03-02 00:51:46 -0800

    Marco-Polo app

---
+++ beginning/js/05-marco-polo.js
@@ -0,0 +1,11 @@
+/* esline-env browser */
+
+for (let i = 0; i <= 100; i++) {
+    if (i % 3 === 0) {     
+        window.console.log('Marco!');
+    } else if (i % 5 === 0) {
+        window.console.log('Polo!');
+    } else {
+        window.console.log('Marco Polo!');
+    }
+  }
