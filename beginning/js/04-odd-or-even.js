b4c1d39e6ccb25048ee772617821232c52f24966
sabrinapea <sabrinapea@yahoo.com>
2021-03-02 00:54:59 -0800

    slight changes

--- beginning/js/04-odd-or-even.js
+++ beginning/js/04-odd-or-even.js
@@ -1,7 +1,7 @@
 /* esline-env browser */
 
 for (let i = 0; i <= 15; i++) {
-  if (i % 2 == 0) {     
+  if (i % 2 === 0) {     
       window.console.log(i + ' is an even number.');
   } else {
     window.console.log(i + ' is an odd number.');
---
+++ beginning/js/06-countdown.js
@@ -0,0 +1,1 @@
+/* esline-env browser */
