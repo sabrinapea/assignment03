ae8297a30b5dcbb656d27ff4cbb1b35a06f4d2c4
sabrinapea <sabrinapea@yahoo.com>
2021-03-03 19:24:41 -0800

    some adjustments in countdown

--- beginning/js/06-countdown.js
+++ beginning/js/06-countdown.js
@@ -1,11 +1,10 @@
 /* esline-env browser */
 
-let number = parseInt(prompt('Enter a number to count down'));
-let i = number;
+let i = parseInt(prompt('Enter a number to count down'));
 do {
     console.log(i); 
     i--;
 } while (i >= 0);
 
 
-// for (let i = parseInt(prompt('Enter a number to count down'));i >= 0; i--) console.log(i);
+// for (let i = parseInt(prompt('Enter a number to count down'));i >= 0; i--) console.log(i); // lean and one line
