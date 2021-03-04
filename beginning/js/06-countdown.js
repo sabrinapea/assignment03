01d89f666c81a3245721b2f30343ad3c448562c8
sabrinapea <sabrinapea@yahoo.com>
2021-03-03 17:19:18 -0800

    Countdown

--- beginning/js/06-countdown.js
+++ beginning/js/06-countdown.js
@@ -1,1 +1,16 @@
 /* esline-env browser */
+
+
+let number = parseInt(prompt('Enter a number to count down'));
+// let i = number;
+// do {
+//     console.log(i); 
+//     i--;
+// } while (i >= 0);
+
+for (let i = number; i > 0; i--) {
+  if (i === 0) {
+    console.log(i); 
+    break;
+  }
+}
